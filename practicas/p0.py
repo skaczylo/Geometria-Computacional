f54b9329e739b5d72aebb483f739cfe5f7d7cf55
skaczylo <acskaaczylo@gmail.com>
2026-04-02 12:01:47 +0200

    practica 0

--- practicas/p0.py
+++ practicas/p0.py
@@ -5,29 +5,76 @@ Instrucciones:
 - Para comprobar su funcionamiento ve al final del código y ejecuta la comprobación correspondiente
 - Sube el código .py a la tarea del CV al final de la clase
 """
-
+import gcom
 from gcom import Punto
 import random
 import math
 import matplotlib.pyplot as plt
 
+def producto_vectorial(a: Punto, b:Punto):
+    """
+    Calcula tercera componente del producto vectorial
+    """
+
+    return a.x*b.y - a.y*b.x
+
+
+def det(a:Punto, b:Punto,c:Punto):
+    return producto_vectorial(b-a,c-a)
 
 def alineados(a: Punto, b: Punto, c: Punto) -> bool:
-    # Devuelve True/False si los puntos a, b, c están alineados/no lo están
-    return False
+    """
+    Devuelve True/False si los puntos a, b, c están alineados/no lo están
+    """
+    #(a,b,c alineados) sii (ab paralelo bc) sii (rectas paralelas) sii (rectas cortan en infinito)
+
+    if abs(det(a,b,c)) < gcom.ERROR:
+        return True
     
+    return False
+
 def ordena_angularmente(puntos: list[Punto]) -> list[Punto]:
-    # Input: puntos es una lista de Punto
-    # Output: lista de puntos ordenada angularmente (según el ángulo desde el origen)
-    # Sugerencia: usar una función de comparación auxiliar como la esbozada
+    """
+    Input: puntos es una lista de Punto
+    Output: lista de puntos ordenada angularmente (según el ángulo desde el origen)
+    Sugerencia: usar una función de comparación auxiliar como la esbozada
+    """
+   
     def angulo(p: Punto) -> float:
+        return math.atan2(p.y,p.x)
+    
+    return sorted(puntos,key=angulo) 
+
+
+def orientacion(a:Punto, b:Punto, c:Punto):
+    """
+    Orientación de 3 puntos.
+    Si orientacion a la izquierda devuelve -1
+    Si coolineares devuelve 0
+    Si orientaacion a la derecha devuelve 1
+    """
+
+    if alineados(a,b,c):
         return 0
-    return 
+    elif det(a,b,c) < 0:
+        return -1
+    else:
+        return 1
+
 
 def punto_en_triangulo(p: Punto, triangulo: list[Punto]) -> bool:
-    # Input: p Punto y triangulo lista con 3 Puntos, los vértices del triángulo
-    # Output: True/False si el punto p está en el interior del triángulo o no    
-    return 
+    """
+    Input: p Punto y triangulo lista con 3 Puntos, los vértices del triángulo
+    Output: True/False si el punto p está en el interior del triángulo o no   
+    """
+    orients = []
+    for i in range(len(triangulo)):
+        orients.append(orientacion(triangulo[i-1],triangulo[i],p))
+
+    if len(set(orients)) == 1: #Misma orientacion para cada lado
+        return True
+    
+    return False
 
 
 def comprueba_alineados(puntos = None):
@@ -65,7 +112,10 @@ def comprueba_punto_en_triangulo (p = None, triangulo = None, respuesta_esperada
     plt.text(p.x, p.y, s)
     plt.show()
 
-# comprueba_alineados([Punto(1,1), Punto(2,2), Punto(3,3)])
-# comprueba_alineados() prueba con 3 puntos de coordenadas enteras pequeñas al azar
+
+
+#comprueba_alineados([Punto(1,1), Punto(2,2), Punto(3,3)])
+comprueba_alineados() #prueba con 3 puntos de coordenadas enteras pequeñas al azar
 # comprueba_ordena_angularmente()
-# comprueba_punto_en_triangulo()
+comprueba_punto_en_triangulo()
+
--- src/gcom.py
+++ src/gcom.py
@@ -1,5 +1,5 @@
 ERROR = 1e-9
-
+import math
 class Punto:
     def __init__(self, x, y):
         self.x = x
@@ -11,3 +11,69 @@ class Punto:
         return Punto(self.x + other.x, self.y + other.y)  
     def __sub__(self, other):
         return Punto(self.x - other.x, self.y - other.y)
+    
+
+def producto_vectorial(a: Punto, b:Punto):
+    """
+    Calcula tercera componente del producto vectorial
+    """
+
+    return a.x*b.y - a.y*b.x
+
+
+def det(a:Punto, b:Punto,c:Punto):
+    return producto_vectorial(b-a,c-a)
+
+def alineados(a: Punto, b: Punto, c: Punto) -> bool:
+    """
+    Devuelve True/False si los puntos a, b, c están alineados/no lo están
+    """
+    #(a,b,c alineados) sii (ab paralelo bc) sii (rectas paralelas) sii (rectas cortan en infinito)
+
+    if abs(det(a,b,c)) < ERROR:
+        return True
+    
+    return False
+
+def ordena_angularmente(puntos: list[Punto]) -> list[Punto]:
+    """
+    Input: puntos es una lista de Punto
+    Output: lista de puntos ordenada angularmente (según el ángulo desde el origen)
+    Sugerencia: usar una función de comparación auxiliar como la esbozada
+    """
+   
+    def angulo(p: Punto) -> float:
+        return math.atan2(p.y,p.x)
+    
+    return sorted(puntos,key=angulo) 
+
+
+def orientacion(a:Punto, b:Punto, c:Punto):
+    """
+    Orientación de 3 puntos.
+    Si orientacion a la izquierda devuelve -1
+    Si coolineares devuelve 0
+    Si orientaacion a la derecha devuelve 1
+    """
+
+    if alineados(a,b,c):
+        return 0
+    elif det(a,b,c) < 0:
+        return -1
+    else:
+        return 1
+
+
+def punto_en_triangulo(p: Punto, triangulo: list[Punto]) -> bool:
+    """
+    Input: p Punto y triangulo lista con 3 Puntos, los vértices del triángulo
+    Output: True/False si el punto p está en el interior del triángulo o no   
+    """
+    orients = []
+    for i in range(len(triangulo)):
+        orients.append(orientacion(triangulo[i-1],triangulo[i],p))
+
+    if len(set(orients)) == 1: #Misma orientacion para cada lado
+        return True
+    
+    return False
